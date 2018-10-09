3799b043bc11e68504400deaa4e101d95082e826
Ronillo Apas Jr <jexos26@gmail.com>
2018-10-10 01:38:30 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95958c391f1bc55a1a96bfa21f60f9e9.js"
+  "/precache-manifest.10cf35ac6a140560a026253e51ca838d.js"
 );
 
 workbox.clientsClaim();
