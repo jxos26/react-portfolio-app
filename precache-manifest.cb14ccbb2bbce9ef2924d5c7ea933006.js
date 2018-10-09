70492312b5b218c7a25f1e738523e928711b12ce
Ronillo Apas Jr <jexos26@gmail.com>
2018-10-10 01:04:24 +0800

    Updates

---
+++ precache-manifest.cb14ccbb2bbce9ef2924d5c7ea933006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "907017fcc705fd2d73a5",
+    "url": "/static/js/main.907017fc.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "907017fcc705fd2d73a5",
+    "url": "/static/css/main.58160222.chunk.css"
+  },
+  {
+    "revision": "1b4e3bef74976ee4c4bdab077afc2c8c",
+    "url": "/index.html"
+  }
+];
